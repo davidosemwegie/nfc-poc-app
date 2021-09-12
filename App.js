1472a084373b00bd635b56e5391c413abff5b39e
David Osemwegie <david@Davids-MacBook-Pro.local>
2021-09-11 18:15:01 -0600

    pull to refresh works

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useEffect, useState} from 'react';
-import { Button, StyleSheet, SafeAreaView } from 'react-native';
+import { Button, StyleSheet, SafeAreaView, RefreshControl, ScrollView } from 'react-native';
 import {readNdef, initNfc} from './utils/read-nfc'
 import {writeNdef} from './utils/write-nfc'
 import axios from 'axios'
@@ -10,24 +10,52 @@ import { Item } from './utils/components/Item';
 export default function App() {
 
   const [links, setLinks] = useState([])
+  const [refreshing, setRefreshing] = React.useState(false);
+
+  const getData = async () => {
+    await axios
+    .get("https://dealership-nfc-poc-api.herokuapp.com/")
+    .then((data) => {
+      console.log(data.data)
+      setLinks(data.data)
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error)
+    })
+  }
+
+  const wait = (timeout) => {
+    return new Promise(resolve => setTimeout(resolve, timeout));
+  }
+
+
+  const onRefresh = React.useCallback(async () => {
+    setRefreshing(true);
+    wait(2000).then(() => {
+      getData()
+      setRefreshing(false)
+    });
+  }, []);
+
 
   useEffect(() => {
-    axios
-      .get("https://dealership-nfc-poc-api.herokuapp.com/")
-      .then((data) => {
-        console.log(data.data)
-        setLinks(data.data)
-      })
-      .catch(function (error) {
-        // handle error
-        console.log(error)
-      })
+    getData()
   }, [])
 
   return (
-    <SafeAreaView style={styles.container}>
+    <SafeAreaView style={styles.container} >
       <Text text70BO style={styles.heading}>Dealership Tags NFC Proof Of Concept</Text>
-      {links.map(({label, url, scanned, id}) => <Item key={id} label={label} scanned={scanned} url={url} />)}
+      <ScrollView
+      refreshControl={
+        <RefreshControl
+        refreshing={refreshing}
+        onRefresh={onRefresh}
+      />
+      }
+      >
+        {links.map(({label, url, scanned, id}) => <Item key={id} label={label} scanned={scanned} url={url} />)}
+      </ScrollView>
     </SafeAreaView>
   );
 }
